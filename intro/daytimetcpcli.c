52a8ac22a8c8bebd80418c542e929815a83dbbcf
rchlin <guglercl@gmail.com>
2015-08-23 08:48:29 +0000

    reformal source code

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -7,25 +7,25 @@ int main(int argc, char *argv[])
 	struct sockaddr_in servaddr;
 
 	if(argc != 2)
-		printf("usage: daytimetcpcli <IPaddress> \n");
+		err_quit("usage: daytimetcpcli <IPaddress>");
 	if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		printf("socket error\n");
+		err_sys("socket error");
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(13);
 	if( inet_pton(AF_INET, argv[1], &servaddr.sin_addr)<=0 )
-		printf("inet_pton error for %s\n", argv[1]);
+		err_quit("inet_pton error for %s", argv[1]);
 	if( connect(sockfd, (SA *) &servaddr, sizeof(servaddr))<0)
-		printf("connect error\n");
+		err_sys("connect error");
 
 	while( (n=read(sockfd, recvline, MAXLINE))>0){
 		recvline[n] = 0;
 		if(fputs(recvline, stdout) ==EOF)
-			printf("fputs error\n");
+			err_sys("fputs error");
 	}
 	if(n<0)
-		printf("read error\n");
+		err_sys("read error");
 
 	exit(0);
 }	
